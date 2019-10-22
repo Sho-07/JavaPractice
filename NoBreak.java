b129c9983d3ed2d48ac1ca1568b3404770deae74
平井将也 <shop4vrd2@gmail.com>
2019-10-23 00:25:44 +0900

    改行せずに文字表示をする命令の学習

---
+++ NoBreak.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]args){
+        String name = "すがわら";
+        System.out.print("私の名前は");
+        System.out.print(name);
+        System.out.print("です");
+        //最後にprintではなくprintlnにしてしまうと
+        //私の名前は
+        //すがわら
+        //です
+        //になってしまうので注意する。
+    }
+}
