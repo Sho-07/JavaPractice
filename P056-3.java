5b3438246e9f5942a80b0328afcbbc1c5b08e2b3
平井将也 <shop4vrd2@gmail.com>
2019-10-23 00:28:53 +0900

    練習問題

---
+++ P056-3.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        boolean result = true;
+        char  Character= "駆";
+        double pi =3.14;
+        long liter =3134535732509503244L;
+        String msg ="ミナトの攻撃、敵に１５のダメージ";
+    }
+}
